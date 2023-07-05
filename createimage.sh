3b8bd5804fbdb9c4f2f8d72b7d49a79cc412f02c
shaileshfc <60957758+shaileshfc@users.noreply.github.com>
2023-07-05 21:56:23 +0200

    Update createimage.sh

--- createimage.sh
+++ createimage.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
-
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 # `-u`: Errors if a variable is referenced before being set
 # `-o pipefail`: Prevent errors in a pipeline (`|`) from being masked
 set -uo pipefail
-
 # Import environment variables from .env
 #set -o allexport && source .env && set +o allexport
-
 echo "Building image '$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG'..."
 # TIP: Add `--progress=plain` to see the full docker output when you are
 # troubleshooting the build setup of your image.
