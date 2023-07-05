847454a98de4c14e8c27257e8992f972a2f8d1e4
shaileshfc <60957758+shaileshfc@users.noreply.github.com>
2023-07-05 23:06:58 +0200

    Update createimage.sh

--- createimage.sh
+++ createimage.sh
@@ -2,18 +2,16 @@
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 # `-u`: Errors if a variable is referenced before being set
 # `-o pipefail`: Prevent errors in a pipeline (`|`) from being masked
-#set -uo pipefail
+set -uo pipefail
 # Import environment variables from .env
-#set -o allexport && source .env && set +o allexport
+set -o allexport && source .env && set +o allexport
 echo "Building image '$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG'..."
 # TIP: Add `--progress=plain` to see the full docker output when you are
 # troubleshooting the build setup of your image.
 #
 # Force x86_64 as the platform.  This workaround is needed on Apple Silicon
 # machines.  Details at https://stackoverflow.com/questions/70736928/.
-#declare -r DOCKER_OPTIONS="--platform linux/x86_64/v8"
+declare -r DOCKER_OPTIONS="--platform linux/x86_64/v8"
 # Use BuildKit, i.e. `buildx build` instead of just `build`
 # https://docs.docker.com/build/
-#docker buildx build $DOCKER_OPTIONS -t "$DOCKER_IMAGE_NAME":"$DOCKER_IMAGE_TAG" .
-ls -ltr
-cat /home/runner/work/_temp/1ec01ba2-3940-485e-ba2e-71d19a2423f3.sh
+docker buildx build $DOCKER_OPTIONS -t "$DOCKER_IMAGE_NAME":"$DOCKER_IMAGE_TAG" .
